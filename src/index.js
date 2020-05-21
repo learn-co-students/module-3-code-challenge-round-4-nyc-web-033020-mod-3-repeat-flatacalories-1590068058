3647c95aee7b71689d3ef0b4d8624776bfe4947e
Vivica <Ma>
2020-05-21 11:38:46 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //√get all names and make it as drowdown menu value
 // √add el change to select a value and show info to //#detailed-info div.
-//add el add calories btn and can + the calories to caharacter default 0 patch the new character data to database 
+//√add el add calories btn and can + the calories to caharacter default 0 patch the new character data to database 
 
 
 //reset calores to make it to 0 and update the calories value to sever 
@@ -13,7 +13,8 @@ document.addEventListener('DOMContentLoaded',e=>{
   const infoDiv = document.querySelector('#detailed-info')
   const img = document.querySelector('#image')
   const p = document.querySelector('#name')
-  const span= document.querySelector('#calories')
+  const span = document.querySelector('#calories')
+ 
   
   const getCharacters = () => {
     fetch(base)
@@ -29,20 +30,42 @@ document.addEventListener('DOMContentLoaded',e=>{
   }
 
   const renderDropdown = character =>{
-      const option = document.createElement('option')
-      option.textContent = character.name
-      option.value = character.id
-      // option.id = character.id
-      select.append(option)
-      select.addEventListener('change', e =>{
-        const id = e.target.value 
-        getAChar(id)
-        .then(char =>{
-          p.textContent = char.name
-          img.src = char.image
-          span.textContent = char.calories
-        })   
-      })
+    const option = document.createElement('option')
+    option.textContent = character.name
+    option.value = character.id
+    select.append(option)
+    
+    select.addEventListener('change', e =>{
+      const id = e.target.value 
+      p.textContent =''
+      getAChar(id)
+      .then(char =>{
+        p.textContent = char.name
+        img.src = char.image
+        span.textContent = char.calories
+        const form  = document.querySelector('#calories-form')
+        form.value= char.id
+        form.addEventListener('submit', e=>{
+          e.preventDefault()
+          let id = e.target.value
+          let calories = parseInt(e.target.children[1].value)
+          
+          let curCal = parseInt(span.textContent)
+          let newCal = curCal + calories
+          console.log(newCal)
+          fetch(`${base}/${id}`,{
+            method: "PATCH",
+            headers: {'content-type': 'application/json',
+                      'accept': 'application/json'
+                      },
+            body: JSON.stringify({calories:newCal})
+          })
+          .then(r=>r.json())
+          .then(getCharacters)
+
+        })
+      })   
+    })
   }
 
   getCharacters()
