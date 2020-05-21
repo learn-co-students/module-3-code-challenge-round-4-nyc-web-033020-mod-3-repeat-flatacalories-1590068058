c1753578ca6a1c01c9d488305548ccda98d00c75
Eliot Henson <eliotrhenson@gmail.com>
2020-05-21 10:21:49 -0400

    3rd commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const url = "http://localhost:3000/characters"
 const dropdown = document.getElementById("character-names")
-console.log(dropdown)
+const characterDiv = document.querySelector("#detailed-info")
+console.log(characterDiv)
 document.addEventListener("DOMContentLoaded", () => {
     getCharacters()
     handleDropdown()
+    handleClick()
 })
 
 const getCharacters = () => {
@@ -16,15 +18,34 @@ const renderCharacters = (characters) => {
     characters.forEach(character => {
        const name = document.createElement("option")
        name.innerText = `${character.name}`
-       name.value = `${character.name}`
-       name.dataset.id = `${character.id}`
+       name.value = `${character.id}`
+    //    name.dataset.id = `${character.id}`
        dropdown.appendChild(name)
     })
 }
 
 const handleDropdown = () => {
     document.addEventListener("change", e => {
-        id = e.target.dataset.id
+        id = e.target.value
+        fetch(`http://localhost:3000/characters/${id}`)
+        .then(res=> res.json())
+        .then(renderCharacter)
 
+    })
+}
+
+const renderCharacter = (character) => {
+    const name = document.getElementById("name")
+    const image = document.getElementById("image")
+    const calories = document.getElementById("calories")
+    name.innerText = `${character.name}`
+    image.src = `${character.image}`
+    calories.innerText = `${character.calories}`
+}
+
+const handleClick = () => {
+    document.addEventListener("submit", e => {
+        e.preventDefault()
+        console.log("hi")
     })
 }
