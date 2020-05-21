dbeb53adfb6a22d302f79bdf2e299eca788e386f
Eliot Henson <eliotrhenson@gmail.com>
2020-05-21 11:51:11 -0400

    done

--- src/index.js
+++ src/index.js
@@ -18,17 +18,17 @@ const renderCharacters = (characters) => {
     characters.forEach(character => {
        const name = document.createElement("option")
        name.innerText = `${character.name}`
-       name.setAttribute("dataset-id", `${character.id}`)
+       name.setAttribute("id", `${character.id}`)
        dropdown.appendChild(name)
-       id = `${character.id}`
+       name.value = `${character.id}`
     
     })
 }
 
 const handleDropdown = () => {
     document.addEventListener("change", e => {
-        const id = e.target.dataset.id
-        fetch(`http://localhost:3000/characters/${id}`)
+        
+        fetch(`http://localhost:3000/characters/${e.target.value}`)
         .then(res=> res.json())
         .then(renderCharacter)
     })
@@ -38,25 +38,26 @@ const renderCharacter = (character) => {
     const name = document.getElementById("name")
     const image = document.getElementById("image")
     const calories = document.getElementById("calories")
-    characterDiv.setAttribute("dataset-id", `${character.id}`)
-    id = `${character.id}`
+    characterDiv.setAttribute("value", `${character.id}`)
+    // id = `${character.id}`
     // const formvalue = document.createElement("input")
     // formvalue.setAttribute("value", `${character.id}`)
     // form.appendChild(formvalue)
     name.innerText = `${character.name}`
     image.src = `${character.image}`
     calories.innerText = `${character.calories}`
-    console.log(characterDiv)
+    // console.log(characterDiv)
 }
 
 
-const handleClick = (id) => {
+const handleClick = () => {
     document.addEventListener("submit", e =>{
         e.preventDefault()
-       
-        console.log(e.target)
-        const formtwo = form.children[1]
+
+
+        id = e.target.parentElement 
+        console.log(id.value)
         
-        console.log("hi")
+       
     })
 } 
