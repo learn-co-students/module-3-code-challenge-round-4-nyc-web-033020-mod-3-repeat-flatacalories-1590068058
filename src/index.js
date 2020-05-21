7d578d45205fb518acf4ec9e143805a1e856634c
Eliot Henson <eliotrhenson@gmail.com>
2020-05-21 11:24:53 -0400

    I realized i put the handleDropdown function inside the forEach but now I cant seem to access e.target.datset in handledropdown

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const characterDiv = document.querySelector("#detailed-info")
 const form = document.getElementById("calories-form")
 document.addEventListener("DOMContentLoaded", () => {
     getCharacters()
-    // handleClick()
+    handleClick()
+    handleDropdown()
 })
 
 const getCharacters = () => {
@@ -20,12 +21,13 @@ const renderCharacters = (characters) => {
        name.setAttribute("dataset-id", `${character.id}`)
        dropdown.appendChild(name)
        id = `${character.id}`
-       handleDropdown(id)
+    
     })
 }
 
-const handleDropdown = (id) => {
+const handleDropdown = () => {
     document.addEventListener("change", e => {
+        const id = e.target.dataset.id
         fetch(`http://localhost:3000/characters/${id}`)
         .then(res=> res.json())
         .then(renderCharacter)
@@ -36,11 +38,25 @@ const renderCharacter = (character) => {
     const name = document.getElementById("name")
     const image = document.getElementById("image")
     const calories = document.getElementById("calories")
-    const formvalue = document.createElement("input")
+    characterDiv.setAttribute("dataset-id", `${character.id}`)
+    id = `${character.id}`
+    // const formvalue = document.createElement("input")
+    // formvalue.setAttribute("value", `${character.id}`)
+    // form.appendChild(formvalue)
     name.innerText = `${character.name}`
     image.src = `${character.image}`
     calories.innerText = `${character.calories}`
+    console.log(characterDiv)
 }
 
 
-  
+const handleClick = (id) => {
+    document.addEventListener("submit", e =>{
+        e.preventDefault()
+       
+        console.log(e.target)
+        const formtwo = form.children[1]
+        
+        console.log("hi")
+    })
+} 
